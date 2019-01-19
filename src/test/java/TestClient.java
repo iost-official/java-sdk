2f70a22cd77df867229662c4f18c710dc1ccbc49
He Peijian <glisxx6@gmail.com>
2019-01-19 17:19:25 +0800

    fix test

--- src/test/java/TestClient.java
+++ src/test/java/TestClient.java
@@ -20,47 +20,68 @@ public class TestClient {
     private Keychain account;
 
     @Test
-    public void testGetNodeInfo() throws IOException {
-        NodeInfo ni = client.getNodeInfo();
-        System.out.println(gson.toJson(ni));
+    public void testGetNodeInfo() {
+        try {
+            NodeInfo ni = client.getNodeInfo();
+            System.out.println(gson.toJson(ni));
+        } catch (IOException e) {
+            System.out.println("network error");
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void testGetChainInfo() throws IOException {
-        ChainInfo ci = client.getChainInfo();
-        System.out.println(gson.toJson(ci));
+    public void testGetChainInfo() {
+        try {
+            ChainInfo ci = client.getChainInfo();
+            System.out.println(gson.toJson(ci));
+        } catch (IOException e) {
+            System.out.println("network error");
+            e.printStackTrace();
+        }
     }
 
-    public void testGetBlockByHash() throws IOException {
-        BlockResponse br = client.getBlockByHash("CgK2PpohVgU3FcNwnopa8ps9NEMBUmBuVKYHuhEe7wBo", true);
-        System.out.println(br.status);
-        System.out.println(gson.toJson(br.block));
+    public void testGetBlockByHash() {
+        try {
+
+            BlockResponse br = client.getBlockByHash("CgK2PpohVgU3FcNwnopa8ps9NEMBUmBuVKYHuhEe7wBo", true);
+            System.out.println(br.status);
+            System.out.println(gson.toJson(br.block));
+        } catch (IOException e) {
+            System.out.println("network error");
+            e.printStackTrace();
+        }
     }
 
     public void testTransfer() throws IOException, TimeoutException {
-        IOST iost = new IOST();
-        Transaction tx = iost.transfer("iost", "admin", "admin", 10.00, ""); //将 10.00 个 iost 从 admin 转账给 receiver
-
         try {
-            this.privateKey = Base58.decode("2yquS3ySrGWPEKywCPzX4RTJugqRh7kJSo5aehsLYPEWkUxBWA39oMrZ7ZxuM4fgyXYs2cPwh5n8aNNpH5x2VyK1");
-        } catch (IOException e) {
+            IOST iost = new IOST();
+            Transaction tx = iost.transfer("iost", "admin", "admin", 10.00, ""); //将 10.00 个 iost 从 admin 转账给 receiver
+
+            try {
+                this.privateKey = Base58.decode("2yquS3ySrGWPEKywCPzX4RTJugqRh7kJSo5aehsLYPEWkUxBWA39oMrZ7ZxuM4fgyXYs2cPwh5n8aNNpH5x2VyK1");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            this.account = new Keychain("admin");
+            KeyPair kp = new Ed25519(privateKey);
+            account.addKey("owner", kp);
+            account.addKey("active", kp);
+
+            this.account.publish(tx);
+
+            String txHash = this.client.sendTx(tx);
+            TxReceipt receipt = this.client.polling(txHash, 1000, 90);
+            if (receipt.status_code.equals("SUCCESS")) {
+                System.out.println("tx success : gas usage " + receipt.gas_usage);
+            } else {
+                System.out.println("tx failed :");
+                System.out.println(receipt.message);
+            }
+        } catch (IOException | TimeoutException e) {
+            System.out.println("network error:");
             e.printStackTrace();
-        }
-
-        this.account = new Keychain("admin");
-        KeyPair kp = new Ed25519(privateKey);
-        account.addKey("owner", kp);
-        account.addKey("active", kp);
-
-        this.account.publish(tx);
-
-        String txHash = this.client.sendTx(tx);
-        TxReceipt receipt = this.client.polling(txHash, 1000, 90);
-        if (receipt.status_code.equals("SUCCESS")) {
-            System.out.println("交易成功，gas使用情况 : " + receipt.gas_usage);
-        } else {
-            System.out.println("交易失败，以下是log：");
-            System.out.println(receipt.message);
         }
     }
 }
